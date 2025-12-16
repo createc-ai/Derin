f87798cf6784ee4d374a3f4eba6b5ac9a0957965
createc-ai <createc.ai.office@gmail.com>
2025-12-16 14:26:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,6 @@ const ctx = canvas.getContext("2d");
 ctx.fillStyle = "#f2f2f2";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-ctx.fillStyle = "#000";
-ctx.font = "24px Inter";
-// ctx.fillText("Createc AI Canvas", 30, 50);
-
 let isDrawing = false;
 
 canvas.addEventListener("mousedown", () => {
